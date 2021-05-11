528936cad2b65c7ab4668e40a9d3751d5733183a
Cmaz1983 <39374805+Cmaz1983@users.noreply.github.com>
2021-05-11 22:36:10 +0100

    Nav Updates

--- core.js
+++ core.js
@@ -1,14 +1,31 @@
 var navigationTrack = false;
-$(document).ready(function(){
-    $('#mobile-nav-open').click(function(){
-        if(navigationTrack){
+let speed = 500;
+$(document).ready(function() {
+    $('#mobile-nav-open').click(function() {
+        if (navigationTrack) {
+            document.getElementsByClassName('global-container')[0].animate([
+                { right: '50%' },
+                { right: '0%' }
+            ], {
+                // timing options
+                duration: speed,
+                iterations: 1,
+                easing: "ease-in-out"
+            });
             document.getElementsByClassName("global-container")[0].style["right"] = "0%";
             navigationTrack = false;
-        }else{
+        } else {
+            document.getElementsByClassName('global-container')[0].animate([
+                { right: '0%' },
+                { right: '50%' }
+            ], {
+                // timing options
+                duration: speed,
+                iterations: 1,
+                easing: "ease-in-out"
+            });
             document.getElementsByClassName("global-container")[0].style["right"] = "50%";
             navigationTrack = true;
         }
-    })    
+    })
 })
-
-
